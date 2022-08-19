94fe502b2d0a9fac42616a04b62c93e507d8feb4
NhLong2109 <111349152+NhLong2109@users.noreply.github.com>
2022-08-19 17:13:24 +0700

    Delete Khai báo biến JS directory

--- Khai báo biến JS/main.js
+++
@@ -1,6 +0,0 @@
-var fullName = ' Huynh Nguyen Nhat Long ';
-var inFormation = ' Sinh ngay 21/9/2003';
-var age = 19;
-console.log(fullName);
-console.warn(inFormation);
-console.error(age + ' XXX Ban chua du tuoi truy cap trang Web XXX');
