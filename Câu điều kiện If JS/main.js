e1a44e03b9c8f4a9341bad478a82bfc06f0985aa
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-20 01:19:15 +0700

    Day la chuong 15

---
+++ Câu điều kiện If JS/main.js
@@ -0,0 +1,11 @@
+var a = 1;
+var b = 5;
+var soSanh = a > b;
+if (soSanh){
+console.log('Phep so sanh dung');
+}
+else{
+    console.warn('Phep so sanh sai');
+}
+
+// trường hợp JS nhận giá trị boolean False là khi vào 5 trường hợp: 0, '' "", false, null, NaN, undefined
