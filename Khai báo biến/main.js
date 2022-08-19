830c95adcf2a76f15cb5f0b3e815e34a17628fa8
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-19 16:54:24 +0700

    Chương 5

--- Khai báo biến/main.js
+++ Khai báo biến/main.js
@@ -1,6 +1,6 @@
 var fullName = ' Huynh Nguyen Nhat Long ';
-var inFormation = ' Sinh ngay 21/9/2003 ';
+var inFormation = ' Sinh ngày 21/9/2003 ';
 var age = 19;
-console.log (fullName);
-console.warn (inFormation);
-console.error (age + ' XXX chưa đủ tuổi đăng nhập JS XXX');
+console.log(fullName);
+console.warn(inFormation);
+console.error(age + ' XXX Bạn chưa đủ tuổi để truy cập JS XXX');
