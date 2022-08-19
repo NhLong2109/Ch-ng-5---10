ea4b05ced96ca917af878ecd5704271bb6e945c6
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-20 00:05:13 +0700

    Day la chuong 9

---
+++ Toán tử số học JS/main.js
@@ -0,0 +1,26 @@
+var a = 1;
+var b = 2;
+console.log(a+b);  //phép cộng
+
+var a = 10;
+var b = 9;
+console.log(a-b);  //phép trừ
+
+var a = 5;
+var b = 2;
+var c = a * b;
+console.log(c);  //phép nhân
+
+var a = 6; b = 3;
+console(a/b);  //phép chia
+
+var a = 9;
+var b = 4;
+c = 9%4;
+console.log(c)  //phép chia dư ( 9 chia 4 dư 1)
+
+var a = 5;
+a++;  // a = 6;
+
+var a = 9;
+a--;  //a = 8
