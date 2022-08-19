255b1e18974cc5f7ebd2acb835d6d648fabbb30c
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-19 17:48:49 +0700

    Day la chuong 6

---
+++ Commets JS/main.js
@@ -0,0 +1,15 @@
+var fullName = ' Huynh Nguyen Nhat Long'; //khai báo họ tên
+var age = 19; //khai báo tuổi
+alert (fullName); //xuất ra cửa sổ thông báo họ tên
+alert(age); //xuất ra cửa sổ thông báo tuổi
+
+/**
+ * comment nhiều dòng theo cú pháp "/** 
+ */
+
+//commet 1 dòng
+
+/*commet
+nhiều dòng */
+
+//phím tắt: Ctrl + /
