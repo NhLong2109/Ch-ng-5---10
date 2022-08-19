c0c0be6f2aa1ad2e45497141aacfdb00b222771c
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-20 00:21:06 +0700

    Day la chuong 10

---
+++ Tiền tố và hậu tố JS/main.js
@@ -0,0 +1,21 @@
+var a = 6;
+++a;
+var output = ++a;
+console.log(++a); // a = 7
+
+var a = 6;
+--a;
+var output = --a;
+console.log(--a); // a = 5
+
+var a = 6;
+a++;
+var output = a++;
+console.log(output); // a = 6
+console.log(a); // a = 7
+
+var a = 6;
+a--;
+var output = a--;
+console.log(output); // a = 6
+console.log(a); // a = 5
