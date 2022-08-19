8cd911a8230ccb7306ce8565b6eca95e174a29d7
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-19 23:53:32 +0700

    Day la chuong 8

--- Làm quen với toán tử JS/main.js
+++ Làm quen với toán tử JS/main.js
@@ -1,23 +1,20 @@
 //toán tử số học
- var number = 1 + 2;
- alert(number);
+var number = 1 + 2;
+alert(number);
 
- //toán tử gán
- var fullName = ' Huynh Nguyen Nhat Long '; //sử dụng dấu " = " để gán cho biến 
+//toán tử gán
+var fullName = ' Huynh Nguyen Nhat Long '; //sử dụng dấu " = " để gán cho biến 
 
- //toán tử so sánh
- var a = 1;
- var b = 2;
- if(a>b){
-    console.log('So sanh dung');
- }
+//toán tử so sánh
+var a = 1;
+var b = 2;
+if(a>b){
+   console.log('So sanh dung');
+}
 
- //toán tử logic
- var a = 9
- var b = 5;
- if(a>0 && b>0){
-    prompt('True')
-    else{
-        prompt('False')
-    }
+//toán tử logic
+var a = 9
+var b = 5;
+if(a>0 && b>0){
+   prompt('True')
  }
