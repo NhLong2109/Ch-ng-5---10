ffbf73427e4593f0a339b7a10385342f23a4d335
longbiicute2109@gmail.com <longbiicute@gmail.com>
2022-08-19 17:37:58 +0700

    Day la chuong 5

---
+++ Khai báo biến JS/main.js
@@ -0,0 +1,8 @@
+var fullName = 'Huynh Nguyen Nhat Long';
+var inFormation = 'Ban sinh ngay 21/9/2003';
+var inFormations = 'Ban chua du 20 tuoi';
+var age = 19;
+console.log(fullName);
+console.warn(inFormation);
+console.warn(inFormations);
+console.error(age + 'tuoi' + ' XXX khong the dang nhap vao JS XXX');
